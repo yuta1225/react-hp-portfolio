b9d6b429c82ae521d715bee39c199a589d9959ef
半田優太 <yuta.h5221@gmail.com>
2025-01-28 22:16:43 +0900

    header adjust

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
-import BlogPage from '../pages/blog';
 import Link from 'next/link';
 
 const Header = () => {
   return (
     <header>
-        <div className='logo'>
-            <h3>
-                <Link href={"/"}>ポートフォリオ</Link>
-            </h3>
-        </div>
+        <div className='wrapper'>
+            <div className='logo'>
+                <h3>
+                    <Link href={"/"}>ポートフォリオ</Link>
+                </h3>
+            </div>
 
-        <nav>
-            <ul>
-                <li>
-                    <Link href={"./#about"}>ABOUT</Link>
-                </li>
-                <li>
-                    <Link href={"./#skills"}>SKILLS</Link>
-                </li>
-                <li>
-                    <Link href={"/contact"}>CONTACT</Link>
-                </li>
-            </ul>
-        </nav>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href={"./#about"}>ABOUT</Link>
+                    </li>
+                    <li>
+                        <Link href={"./#skills"}>SKILLS</Link>
+                    </li>
+                    <li>
+                        <Link href={"/contact"}>CONTACT</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </header>
   )
 }
