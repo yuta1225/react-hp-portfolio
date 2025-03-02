ce6b5cb6e20b2339ccd9f212485f92d621678661
半田優太 <yuta.h5221@gmail.com>
2025-03-02 12:12:59 +0900

    js amination base

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer"
+import Script from "next/script"; //jsファイルをグローバルに使用する
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -11,6 +12,7 @@ type LayoutProps = {
 export default function Layout({ children, title, description }: LayoutProps) {
   return (
     <div id="App">
+      <Script src="js/animation.js" strategy="afterInteractive"></Script>
       <Head>
         <title>{title || "YUTA HANDA｜エンジニアポートフォリオ"}</title>
         <meta name="description" content={description || "エンジニアポートフォリオサイトです。新卒でIT企業に就職し、SE/PGとして主に製造業の基幹系システム開発に約4年間携わりました。AWS/GCP/Azureなどのパブリッククラウドにも興味があり日々技術習得中です。本ポートフォリオサイトは、私の現在のITスキルや経験内容を見える化してまとめたいと考え制作しました。3ヶ月毎にスキル棚卸しのためにサイト更新しています。"} />
